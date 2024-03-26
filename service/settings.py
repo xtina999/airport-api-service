ff9413ef60b64e91afe7d1d4e779d44194828636
Kristina <tina.vasylenko@gmail.com>
2024-03-26 11:08:11 +0200

    add 'rest_framework' to INSTALLED_APPS

--- service/settings.py
+++ service/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "airport"
+    "rest_framework",
+    "airport",
 ]
 
 MIDDLEWARE = [
