ab945d7e6da96094857e0da76a418ba94340ff8c
Kristina <tina.vasylenko@gmail.com>
2024-03-27 16:07:09 +0200

    Updated urls.py in service app && added db.sqlite3 to .gitignore

--- service/urls.py
+++ service/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/airplane/", include("airplane.urls", namespace="airplane"))
+    path("api/airport/", include("airport.urls", namespace="airport"))
 ]
