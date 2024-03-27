54e9042afea309a9df1d5c61cdcacc9a4131a077
Kristina <tina.vasylenko@gmail.com>
2024-03-27 15:48:44 +0200

    Added viewsets for City, AirplaneType, Airport, Route, Flight, Ticket, and Order models to airport/views.py

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    AirplaneType,
+    Airport,
+    Airplane,
+    Flight,
+    Route,
+    Order,
+    Ticket,
+    City,
+
+)
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
