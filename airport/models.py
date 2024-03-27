14e6cf5b4532f78b4e37f4081e8beb843be12f62
Kristina <tina.vasylenko@gmail.com>
2024-03-27 11:57:22 +0200

    Added City model
    Added City model with the name field representing the city name. Implemented the __str__ method for convenient display. Added to airport/models.py.

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=64)
+
+    class Meta:
+        verbose_name_plural = "cities"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
