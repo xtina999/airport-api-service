c8616ae05812103cfef5afb8ace09316647a2dcf
Kristina <tina.vasylenko@gmail.com>
2024-03-27 15:55:45 +0200

    Added serializer_class to all models in airport/serializers

--- airport/views.py
+++ airport/views.py
@@ -8,38 +8,62 @@ from airport.models import (
     Route,
     Order,
     Ticket,
-    City,
-
+    Crew,
+    City
+)
+from airport.serializers import (
+    CitySerializer,
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    AirportSerializer,
+    RouteSerializer,
+    FlightSerializer,
+    TicketSerializer,
+    OrderSerializer,
+    CrewSerializer
 )
 
 
 class CityViewSet(viewsets.ModelViewSet):
     queryset = City.objects.all()
+    serializer_class = CitySerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
 
 
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
 
 
 class AirplaneViewSet(viewsets.ModelViewSet):
     queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
 
 
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all()
+    serializer_class = RouteSerializer
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
+    serializer_class = OrderSerializer
