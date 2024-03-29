4c1f587f2d27279b50f1fa72ff761f80ae82a62f
Kristina <tina.vasylenko@gmail.com>
2024-03-29 10:16:29 +0200

    Added django-debug-toolbar for easier debugging and database query optimization

--- service/settings.py
+++ service/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@e27unrxmfu!s#_rs+dzj(@+we6)5lz$44-g8cqkk4y(g^o9z!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -39,12 +39,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "airport",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -103,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
--- service/urls.py
+++ service/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/airport/", include("airport.urls", namespace="airport"))
+    path("api/airport/", include("airport.urls", namespace="airport")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
