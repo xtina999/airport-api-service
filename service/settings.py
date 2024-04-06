93438186c5143c2bab1b596d2b0cb252d60d1587
Kristina <tina.vasylenko@gmail.com>
2024-04-06 18:48:50 +0300

    Created package for tests and added test cases
    Created package for tests and added test cases for listing airplanes, listing airports, and filtering airports by city

--- service/settings.py
+++ service/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
+import sys
 from datetime import timedelta
 from pathlib import Path
+
+import airport
+from airport import tests
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +137,11 @@ STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
+
+TESTS_DIR = os.path.join(BASE_DIR, 'airport', 'tests')
+sys.path.append(TESTS_DIR)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
