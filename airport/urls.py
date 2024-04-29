ff083df18a394f499280e1a561024099e52beac7
xtina999 <avgustroder@gmail.com>
2024-04-29 15:58:39 +0300

    Simplifying urls.py

--- airport/urls.py
+++ airport/urls.py
@@ -28,8 +28,6 @@ router.register("order", OrderViewSet)
 router.register("crew", CrewViewSet)
 
 
-urlpatterns = [
-    path("", include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "airport"
